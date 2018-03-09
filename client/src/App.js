c8cae8b4da2ce8f3e23ad6ff1498268df025317c
hoslack <hoslackochieng@gmail.com>
2018-03-09 15:25:12 +0300

    initialize react and node app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import 'materialize-css/dist/js/materialize.min.js';
+import 'materialize-css/dist/css/materialize.min.css';
+
+class App extends Component {
+  render() {
+    return <div className="App">Welcome</div>;
+  }
+}
+
+export default App;
